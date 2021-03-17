f43d84e0bba9d589d210afcc71c4379b5cdf21fa
mehmetunalb <76492069+mehmetunalb@users.noreply.github.com>
2021-03-16 20:26:29 -0500

    Mehmet push 2

--- src/Branches.java
+++ src/Branches.java
@@ -2,12 +2,14 @@ public class Branches {
     public static void main(String[] args) {
         System.out.println("step 1");
 
-        System.out.println("Mehmet");
 
-        System.out.println("Guitar Center and Rock and Roll");
 
-        System.out.println("Serhat");
-        System.out.println("Ayhan Abi");
 
+
+
+
+
+
+        System.out.println("mehmet second push");
     }
 }
