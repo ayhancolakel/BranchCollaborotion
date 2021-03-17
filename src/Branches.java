07700a4e883af3efb862954f11f97576e2ccbcb8
armaganaydingul <76491936+armaganaydingul@users.noreply.github.com>
2021-03-16 20:17:46 -0500

    added my name

--- src/Branches.java
+++ src/Branches.java
@@ -2,5 +2,8 @@ public class Branches {
     public static void main(String[] args) {
         System.out.println("step 1");
 
+        System.out.println("Serhat");
+        System.out.println("Ayhan Abi");
+        System.out.println("armagan");
     }
 }
