a650afc3b94dc1ffffedc9023124351078cb18ea
Renas63 <75856707+Renas63@users.noreply.github.com>
2021-03-16 20:27:57 -0500

    new commit for all

--- src/Branches.java
+++ src/Branches.java
@@ -2,5 +2,6 @@ public class Branches {
     public static void main(String[] args) {
         System.out.println("step 1");
         System.out.println("Renas");
+        System.out.println("Marsa");
     }
 }
