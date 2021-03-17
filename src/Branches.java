7db047cbd670de91795fd111f1242eec0c30f11e
ayhancolakel <colakelayhan@gmail.com>
2021-03-16 20:18:31 -0500

    Branch First push 2

--- src/Branches.java
+++ src/Branches.java
@@ -2,12 +2,5 @@ public class Branches {
     public static void main(String[] args) {
         System.out.println("step 1");
 
-        System.out.println("Mehmet");
-
-        System.out.println("Guitar Center and Rock and Roll");
-
-        System.out.println("Serhat");
-        System.out.println("Ayhan Abi");
-
     }
 }
